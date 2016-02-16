0f46fa5bea2627ba1c217fbd42b01339bafe1771
Julie Lerman <julielerman@gmail.com>
2016-02-16 14:37:16 -0500

    Added MigrationContextFactory
    this lets you use ef migrations even though the context doesn't have a
    constructor because we're using IOC

---
+++ src/EF6Model/Migrations/MigrationsContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Model.Migrations
+{
+  public class MigrationsContextFactory : IDbContextFactory<NinjaContext>
+  {
+    public NinjaContext Create() {
+      return new NinjaContext("Server = (localdb)\\mssqllocaldb; Database = NinjaContext; Trusted_Connection = True; MultipleActiveResultSets = true");
+    }
+  }
+}
